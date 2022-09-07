d3ec25e08811dc14ce86299342e6266da80f0863
Rikkiyleader <110509017+addmaster1@users.noreply.github.com>
2022-09-07 14:57:40 +1000

    Addition
    Added multiple picture input and output in console

--- javascript/index.js
+++ javascript/index.js
@@ -1,21 +1,63 @@
 const catImg = document.getElementById("CAT1");
+var numberOfImages = document.getElementById("numberOfImages").value;
+var submitButton = document.getElementById("submit");
+var cat;
+var selRandom = [];
+var ids = [];
 
-//* function to fetch data from API
+//? Fetching multiple images from API
+async function fCatImages(numberofImages) {
+	// * fetching the api
+	var catImage = await fetch("https://cataas.com/api/cats");
+
+	//* converting to json format
+	catImage = await catImage.json();
+	console.log(catImage);
+	console.log(catImage.length);
+
+	//? Picks random number within the range of the catImage.length / pushes them to selRandom
+	for (let i = 0; i < numberOfImages; i++) {
+		var unselRandom = Math.floor(Math.random() * catImage.length);
+		selRandom.push(unselRandom);
+	}
+
+	//? for loop to replace ids with urls
+	for (let i = 0; i < selRandom.length; i++) {
+		console.log(selRandom[i]);
+		selRandom[i] = "https://cataas.com/cat?" + catImage[i].id;
+		console.log(selRandom);
+
+		// imageDisplay(selRandom);
+	}
+
+	//! id for image of random cat TESTING
+	// console.log(catImage[8].id);
+	// console.log(selRandom);
+}
+
+//* function to fetch 1 image from API
 async function getCatImage() {
-	let catImages = await fetch("https://cataas.com/cat");
+	let catImage = await fetch("https://cataas.com/cat");
 
 	// console.log(catImages);
-	insertCatImg(catImages);
+	insertCatImg(catImage);
 }
 
 //* function to insert image into document
-function insertCatImg(catImages) {
+function insertCatImg(catImage) {
 	console.log(catImg);
-	console.log(catImages);
+	console.log(catImage);
 
 	// * * Pulling out the url out of the JSON formatted response from API and displaying it
-	catImg.src = catImages.url;
+	catImg.src = catImage.url;
 	console.log(catImg);
 }
 
-getCatImage();
+// getCatImage();
+
+//? Testing input and submit button
+submitButton.addEventListener("click", getValue);
+
+function getValue() {
+	fCatImages(numberOfImages);
+}
