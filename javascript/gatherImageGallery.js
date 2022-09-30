03e23c8fd8f9bd68c9cfb9453dca80d445d0f8e8
Rikkiyleader <110509017+addmaster1@users.noreply.github.com>
2022-10-01 01:22:46 +1000

    File Naming change

--- javascript/gatherImageGallery.js
+++ javascript/gatherImageGallery.js
@@ -1,3 +1,54 @@
+// SECTION
+// PURPOSE Is to gather image data for gallery and to sort still and Gif images. Also to gather random image numbers that depend on the type of image that the user requests
+
+// 1. Async Function Fetching Data - To gather the data and storing it into an array = data
+// 2. Sorting - To sort data into two global arrays stillImages + gifImages
+// 3. Random Numbers - To gather random numbers depending on how many they request
+// !SECTION
+
+// SECTION Async Function Fetching Data
+async function getImages() {
+	// NOTE Setting all arrays to 0 length so there is no repeat
+	stillImages.length = 0;
+	gifImages.length = 0;
+
+	// NOTE Fetching Data
+	var fetchData = await fetch("https://cataas.com/api/cats");
+	data = await fetchData.json(); // NOTE Changing to json format
+	creatingSortedData.sort(); // run next class - sorting
+}
+// !SECTION
+
+// SECTION Sorting data array into two different arrays stillImages + gifImages
+const creatingSortedData = {
+	sort() {
+		// NOTE Setting yesStill to zero so data is always reset
+		var yesStill = 0;
+
+		// NOTE To sort STILL or GIF
+		for (let a = 0; a < data.length; a++) {
+			for (let b = 0; b < data[a].tags.length; b++) {
+				console.log[a];
+				// NOTE Checks if there is a gif in array
+				if (data[a].tags[b] != "gif") {
+					yesStill = yesStill + 1;
+				}
+			}
+			// NOTE checking whether - ...tags.length == yesStill
+			if (yesStill == data[a].tags.length) {
+				stillImages.push(data[a]); // puts Still Images in with stillImages
+			} else {
+				// NOTE checking wheather - ...tags.length != Still
+				gifImages.push(data[a]); // puts GIFs in with gifImages
+			}
+			// NOTE To reset yesStill
+			yesStill = 0;
+		}
+		getRandom.numbers();
+	},
+};
+// !SECTION
+
 //! GET RANDOM NUMBERS
 const getRandom = {
 	numbers() {
@@ -23,39 +74,6 @@ const getRandom = {
 	},
 };
 
-// SECTION fetching IMAGES
-// TOPIC ASYNC FETCHING Images ASYNC
-async function getImages() {
-	var yesStill = 0;
-	stillImages.length = 0;
-	console.log(yesStill);
-	var fetchData = await fetch("https://cataas.com/api/cats");
-	data = await fetchData.json();
-
-	// NOTE To sort STILL or GIF
-	for (let a = 0; a < data.length; a++) {
-		for (let b = 0; b < data[a].tags.length; b++) {
-			console.log[a];
-			// NOTE Checks if there is a gif in array
-			if (data[a].tags[b] != "gif") {
-				yesStill = yesStill + 1;
-			}
-		}
-
-		// NOTE checking whether - ...tags.length == yesStill
-		if (yesStill == data[a].tags.length) {
-			stillImages.push(data[a]); // puts Still Images in with stillImages
-		} else {
-			// NOTE checking wheather - ...tags.length != Still
-			gifImages.push(data[a]); // puts GIFs in with gifImages
-		}
-		// NOTE To reset yesStill
-		yesStill = 0;
-	}
-	getRandom.numbers();
-}
-// !SECTION
-
 const Image_GIF = {
 	imageGifDesign(noImage) {
 		GIFQueue.length = 0;
@@ -66,6 +84,9 @@ const Image_GIF = {
 			GIFQueue[i] = "https://cataas.com/cat/" + gifImages[randomNumberSet[i]].id;
 		}
 
+		// TODO Complete say element
+		// NOTE Getting value of SAY text input + storing in var
+		// VAR
 		console.log(GIFQueue);
 
 		if (size.value == "or") {
@@ -83,6 +104,8 @@ const Image_GIF = {
 				GIFQueue[i] = GIFQueue[i] + "?type=" + size.value;
 			}
 		}
+
+		// TODO Complete color for say
 		console.log(GIFQueue);
 
 		Image_GIF.imageGIFDefine();
